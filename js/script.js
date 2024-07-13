ecab74c11d15298c27677bd608e60722cfee25c6
Ganesh Krishnan <ganesh.krishnan13@gmail.com>
2024-07-13 20:08:15 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 const images = [
-    'img/profile1.jpg',
-    'img/profile2.jpg',
-    'img/profile4.jpg',
-    'img/profile7.jpg',
-    'img/profile8.jpg',
-    'img/profile9.jpg',
-    'img/profile10.jpg',
-    'img/profile11.jpg',
     'img/profile12.jpg'
 ];
 
